129813f78565c9432ddf92d5739fa4407c4c9e9c
saurabh.yagnik <saurabh.yagnik@CPX-AH6W9IBXTNM.dir.svc.accenture.com>
2019-06-11 16:22:35 +0800

    refacored html

--- estt-web/src/main/resources/static/js/app/EndPointStatusService.js
+++ estt-web/src/main/resources/static/js/app/EndPointStatusService.js
@@ -7,5 +7,9 @@ angular.module('dashboard.services', []).factory('EndPointStatusService',
 				var url = CONSTANTS.getEpStatusForPieChart + userId;
 				return $http.get(url);
 			}
+			service.deleteStatusFile = function(userId) {
+				var url = CONSTANTS.deleteStatusFile + userId;
+				return $http.get(url);
+			}
 			return service;
 		} ]);
