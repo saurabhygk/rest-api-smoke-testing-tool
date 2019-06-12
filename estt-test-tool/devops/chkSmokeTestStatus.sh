89cf0acda2b0059580a312fc926d2d1c5cd6fa12
saurabh.yagnik <saurabh.yagnik@CPX-AH6W9IBXTNM.dir.svc.accenture.com>
2019-06-12 12:34:23 +0800

    changed the shell script which will execute jar and check status file as well

--- estt-test-tool/devops/chkSmokeTestStatus.sh
+++ estt-test-tool/devops/chkSmokeTestStatus.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
-statusFilePath=${STATUS_FILE_PATH}
+echo "Location of estt smoke test tool program location: $1"
+echo "Location of coniguration files: $2"
+echo "User Id: $3"
 
-if [ -f $statusFilePath/status.txt ]; then
- # grep command with -c will give the number of lines having "ERROR ->" word. if having more than 1 then exit with 1
- command=`grep -c 'ERROR ->' $statusFilePath/status.txt` 
- if [ "$command" -ge 1 ]; then
-  echo "Found ERROR(S) smoke testing, please check $statusFilePath/status.txt file for more detail."
+if [ -z "$1" ]
+then
+  echo "Please provide first estt smoke test tool program location."
   exit 1
- fi
-else 
- echo "There is some issue while executing smoke test tool during creation of status.txt. please check tool logs..!!"
- exit 1 
+fi
+
+if [ -z "$2" ]
+then
+ echo "Please proivde second argument location of configuration files"	
+ exit
+fi
+
+if [ -z "$3" ]
+then
+ echo "Please provide third argument User Id";
+ exit 1
+fi
+
+esttProg=$1/endpoint-smoketest-tool.jar
+statusFile=$2/status_$3.txt
+
+if [ -f $esttProg ]; then
+	java -jar $esttProg $2 $3
+
+	if [ -f $statusFile ]; then
+	 # grep command with -c will give the number of lines having "ERROR ->" word. if having more than 1 then exit with 1
+	 command=`grep -c 'ERROR ->' $statusFile` 
+	 if [ "$command" -ge 1 ]; then
+	  echo "Found ERROR(S) smoke testing, please check $statusFile file for more detail."
+	  exit 1
+	 fi
+	else 
+	 echo "There is some issue while executing smoke test tool."
+	 exit 1 
+	fi
+else
+ echo "ESTT executable jar not found at location $1"
+ exit 1
 fi
 exit 0
